a0d8effc2890b5735de061cabd25b59703f36b87
Nick <zhong.ren@hotmail.com>
2026-05-04 17:13:57 -0400

    test(study): Avoid brittle credible set filter

--- tests/test_study_tools.py
+++ tests/test_study_tools.py
@@ -70,9 +70,7 @@ class TestStudyTools:
                 assert "colocalisation" in coloc_result["credibleSet"]
 
     async def test_get_credible_sets(self, client: OpenTargetsClient):
-        result = await self.study_api.get_credible_sets(
-            client, study_types=["gwas"], page_size=2
-        )
+        result = await self.study_api.get_credible_sets(client, page_size=2)
         assert result is not None
         assert "credibleSets" in result
         if result.get("credibleSets"):
